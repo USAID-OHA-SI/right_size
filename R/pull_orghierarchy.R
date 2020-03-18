ee0617165b174076db7ff3fd281c6a337fb60fe0
Aaron Chafetz <achafetz@usaid.gov>
2020-03-18 17:33:42 -0400

    add fcn to pull heirachy from DATIM

---
+++ R/pull_orghierarchy.R
@@ -0,0 +1,33 @@
+##  PROJECT: RIGHT SIZE
+##  AUTHOR:  A.Chafetz | USAID
+##  PURPOSE: pull DATIM hierarchy
+##  LICENCE: MIT
+##  DATE:    2020-03-18
+##  UPDATE:  
+
+
+# DEPENDENCIES ------------------------------------------------------------
+
+library(tidyverse)
+library(Wavelength)
+library(lubridate)
+
+
+# GLOBAL VARIABLES --------------------------------------------------------
+
+  myuser <- ""
+
+
+# PULL OU HIERARCHY -------------------------------------------------------
+
+  #identify uids
+    ouuids <- identify_ouuids(myuser, mypwd(myuser)) %>% dplyr::pull(id)
+    
+  #pull hierarchy
+    df_orgs <- purrr::map_dfr(.x = ouuids,
+                              .f = ~ pull_hierarchy(.x, myuser, mypwd(myuser)))
+
+# EXPORT ------------------------------------------------------------------
+
+  #save
+    hfr_export(df_orgs, "Data", type = "orghierarchy")
