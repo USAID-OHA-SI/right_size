32594e4b80b6dbf0ac593ca569bca4a9e0550525
Aaron Chafetz <achafetz@usaid.gov>
2020-03-29 12:18:28 -0400

    add flag for lone reporting & merge on mech/org meta

--- R/site_transitions.R
+++ R/site_transitions.R
@@ -3,99 +3,126 @@
 ##  PURPOSE: flag site shifts
 ##  LICENCE: MIT
 ##  DATE:    2020-03-18
-##  UPDATE:  
+##  UPDATE:  2020-03-29
 
 
 # DEPENDENCIES ------------------------------------------------------------
 
 library(tidyverse)
 library(vroom)
+library(fs)
+library(ICPIutilities)
 
 # IMPORT ------------------------------------------------------------------
 
-df <- vroom("Data/TX_CURR_IM_Site.csv", col_types = c(.default = "c", value = "d"))
+  #read data
+    df <- vroom("Data/TX_CURR_IM_Site.csv", col_types = c(.default = "c", value = "d"))
 
 
 # MUNGE -------------------------------------------------------------------
 
-df <- df %>% 
-  arrange(operatingunit, orgunituid, period) %>% 
-  select(operatingunit, orgunituid, period, mech_code, fundingagency, value)
-
-#flag multi mechanism site
-  df <- df  %>% 
-    group_by(orgunituid, period) %>% 
-    mutate(flag_multimech_site = n() > 1) %>% 
-    ungroup()
-
-#identify last site obs
-  df <- df %>% 
-    group_by(orgunituid) %>% 
-    mutate(last_obs_site = max(period)) %>% 
-    ungroup() 
+  #store hierarchy for later
+    df_org <- df %>% 
+      distinct(operatingunit, countryname, snu1, psnu, facility, orgunituid)
+    
+  #flag for lone obs 
+    df <- df %>% 
+      complete(period, nesting(operatingunit, orgunituid, mech_code, fundingagency)) %>% 
+      arrange(orgunituid, mech_code, period) %>% 
+      group_by(orgunituid, mech_code) %>% 
+      mutate(flag_loneobs = !is.na(value) & is.na(lead(value, order_by = period)) & is.na(lag(value, order_by = period))) %>% 
+      ungroup() %>% 
+      mutate(flag_loneobs = ifelse(period %in% c("FY17Q4","FY20Q1"), FALSE, flag_loneobs)) %>% 
+      filter(!is.na(value))
+    
+  #limit df for analysis
+    df <- df %>% 
+      arrange(operatingunit, orgunituid, period) %>% 
+      select(operatingunit, orgunituid, period, mech_code, fundingagency, value)
+    
+  #flag multi mechanism site
+    df <- df  %>% 
+      group_by(orgunituid, period) %>% 
+      mutate(flag_multimech_site = n() > 1) %>% 
+      ungroup()
   
-#flag sitexmech end
-  df <- df %>% 
-    group_by(orgunituid, mech_code) %>% 
-    mutate(last_obs_sitexmech = max(period)) %>% 
-    ungroup() %>% 
-    mutate(flag_end_sitexmech = period == last_obs_sitexmech & period != "FY20Q1")
-
-#end type
-  df <- df %>% 
-    group_by(orgunituid) %>% 
-    mutate(end_type = case_when(flag_end_sitexmech == TRUE & last_obs_sitexmech == last_obs_site ~ "Transition out of PEPFAR",
-                                flag_end_sitexmech == TRUE & flag_multimech_site == TRUE ~ "Consolidate mutli-mechanism site",
-                                flag_end_sitexmech == TRUE & fundingagency != lead(fundingagency, order_by = period) ~ "Transition to other agency",
-                                flag_end_sitexmech == TRUE & mech_code != lead(mech_code, order_by = period) ~ "Transition to other mechanism")) %>% 
-    ungroup()
+  #identify last site obs
+    df <- df %>% 
+      group_by(orgunituid) %>% 
+      mutate(last_obs_site = max(period)) %>% 
+      ungroup() 
+    
+  #flag sitexmech end
+    df <- df %>% 
+      group_by(orgunituid, mech_code) %>% 
+      mutate(last_obs_sitexmech = max(period)) %>% 
+      ungroup() %>% 
+      mutate(flag_end_sitexmech = period == last_obs_sitexmech & period != "FY20Q1")
   
-#identify agency transitions
-  df <- df %>% 
-    group_by(orgunituid) %>%
-    mutate(agency_exiting = case_when(end_type == "Transition to other agency" ~ fundingagency),
-           agency_inheriting = case_when(end_type == "Transition to other agency" ~ lead(fundingagency, order_by = period))) %>% 
-    ungroup()
+  #end type
+    df <- df %>% 
+      group_by(orgunituid) %>% 
+      mutate(end_type = case_when(flag_end_sitexmech == TRUE & last_obs_sitexmech == last_obs_site ~ "Transition out of PEPFAR",
+                                  flag_end_sitexmech == TRUE & flag_multimech_site == TRUE ~ "Consolidate mutli-mechanism site",
+                                  flag_end_sitexmech == TRUE & fundingagency != lead(fundingagency, order_by = period) ~ "Transition to other agency",
+                                  flag_end_sitexmech == TRUE & mech_code != lead(mech_code, order_by = period) ~ "Transition to other mechanism")) %>% 
+      ungroup()
+    
+  #identify agency transitions
+    df <- df %>% 
+      group_by(orgunituid) %>%
+      mutate(agency_exiting = case_when(end_type == "Transition to other agency" ~ fundingagency),
+             agency_inheriting = case_when(end_type == "Transition to other agency" ~ lead(fundingagency, order_by = period))) %>% 
+      ungroup()
 
 
-# MERGE -------------------------------------------------------------------
+# MERGE META --------------------------------------------------------------
 
   #import mechanism info
-    df_org <- list.files("Data", "orghierarchy", full.names = TRUE) %>% 
-      vroom(col_types = c(.default = "c")) %>% 
-      select(-c(latitude, longitude, orgunit, level))
+    df <- rename_official(df)
+    
+  #reorder
+    df <- df %>% 
+      select(operatingunit, orgunituid, fundingagency, 
+             mech_code, mech_name, primepartner, everything())
+    
+  #bring org hierarchy data in
+    df <- df %>% 
+      select(-operatingunit) %>% 
+      right_join(df_org, ., by = "orgunituid")
+    
+    rm(df_org)
   
-  #merge
-    df_full <- df %>% 
-      select(-operatingunit) %>%
-      right_join(df_org, .)
-    
-    
 
 # EXPORT ------------------------------------------------------------------
 
-  dir.create("Dataout")  
+  dir_create("Dataout")  
+    
+  #export all
+    write_csv(df, "Dataout/TXCURR_Flags.csv", na = "")
   
   #all ending sites
     df_end <- df_full %>% 
       filter(fundingagency != "Dedupe adjustments Agency",
              flag_end_sitexmech == TRUE,
              period != "FY17Q4")
+    
+    write_csv(df_end, "Dataout/TXCURR_SiteTransitions.csv", na = "")
+    
   #all transitioning out of PEPFAR
     df_end_out <- df_end %>% 
       filter(end_type == "Transition out of PEPFAR") %>% 
       select(orgunituid:value) 
+    
+    write_csv(df_end_out, "Dataout/TXCURR_SiteTransitions_OutofPEPFAR.csv", na = "")
+    
   #all multi mechanism sites
     df_multimechs <- df_full %>% 
       filter(flag_multimech_site == TRUE) %>% 
       select(orgunituid:flag_multimech_site) 
     
-  #export
-    write_csv(df_end_out, "Dataout/TXCURR_SiteTransitions_OutofPEPFAR.csv", na = "")
-    write_csv(df_end, "Dataout/TXCURR_SiteTransitions.csv", na = "")
     write_csv(df_multimechs, "Dataout/TXCURR_Site_MultiMechs.csv", na = "")
-    
-  
+
 
 # EXPLORE -----------------------------------------------------------------
 
