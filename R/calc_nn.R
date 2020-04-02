12a6cf0731f13d67cc1b74a4866844c9ecf71fbf
Aaron Chafetz <achafetz@usaid.gov>
2020-04-02 16:51:58 -0400

    add 'real' site tx_curr, fill missing cols from complete, rename dfs

--- R/calc_nn.R
+++ R/calc_nn.R
@@ -27,44 +27,56 @@ library(ICPIutilities)
       pull()
     
   #remove all sites historically where there any instance of having muli-mechs
-    df <- filter(df, !orgunituid %in% lst_multimech_site)
+    df_sngl <- filter(df, !orgunituid %in% lst_multimech_site)
     
   #rename value to tx_curr
-    df <- rename(df, tx_curr = value)
+    df_sngl <- rename(df_sngl, tx_curr = value)
     
   #create a full set of periods for calculating NET NEW
-    df <- df %>% 
+    df_full <- df_sngl %>% 
       mutate(tx_curr_w_zero = tx_curr) %>% 
       complete(period, nesting(orgunituid, mech_code), fill = list(tx_curr_w_zero = 0)) %>% 
       arrange(operatingunit, orgunituid, mech_code, period)
     
   #calc normal NET_NEW
-    df <- df %>%
+    df_nn <- df_full %>%
       group_by(orgunituid, mech_code) %>% 
       mutate(tx_net_new = tx_curr_w_zero - lag(tx_curr_w_zero, order_by = period)) %>% 
       ungroup()
       
   #calc adjusted NET_NEW
-    df <- df %>% 
+    df_nn <- df_nn %>% 
       group_by(orgunituid) %>% 
       mutate(tx_net_new_adj = tx_curr_w_zero - lag(tx_curr_w_zero)) %>% 
       ungroup()
-  
-  #remove tx_curr w/ zero instead of NA
-    df <- df %>% 
-      select(-tx_curr_w_zero) %>% 
-      filter(!is.na(tx_curr))
+    
+  #create a lag TX_CURR var
+    df_nn <- df_nn %>% 
+      group_by(orgunituid) %>% 
+      mutate(tx_curr_site_lag = lag(tx_curr)) %>% 
+      ungroup()
       
+  #remove tx_curr w/ zero and any with all 0/NAs
+    df_nn <- df_nn %>% 
+      select(-tx_curr_w_zero) %>% 
+      filter_at(vars(tx_curr, tx_net_new, tx_net_new_adj), 
+                any_vars(!is.na(.) & . != 0))
 
-  site_uid <- "KNVM48HI1Qn" #"RSCJ3wnhmYf"
+  #fill extra lines where blank due to complete
+    df_nn <- df_nn %>% 
+      group_by(orgunituid) %>% 
+      fill(operatingunit:primepartner, flag_loneobs:flag_end_sitexmech) %>% 
+      ungroup()
+    
   #check
-    df %>% 
-      filter(orgunituid == site_uid) %>% 
-      select(period, facility, mech_code, tx_curr, tx_net_new, tx_net_new_adj) %>% 
-      gather(indicator, value, tx_curr, tx_net_new, tx_net_new_adj) %>% 
-      mutate(indicator = factor(indicator, c("tx_curr", "tx_net_new", "tx_net_new_adj"))) %>% 
-      spread(period, value)
+    # site_uid <- "RSCJ3wnhmYf"  #"HaDaUb79d7g"  #"KNVM48HI1Qn" #"RSCJ3wnhmYf"
+    # df_nn %>%
+    #   filter(orgunituid == site_uid) %>%
+    #   select(period, facility, mech_code, tx_curr, tx_net_new, tx_net_new_adj) %>%
+    #   gather(indicator, value, tx_curr, tx_net_new, tx_net_new_adj) %>%
+    #   mutate(indicator = factor(indicator, c("tx_curr", "tx_net_new", "tx_net_new_adj"))) %>%
+    #   spread(period, value)
     
 # EXPORT ------------------------------------------------------------------
 
-    write_csv(df, "Dataout/TX_CURR_NN_Calcs.csv", na = "")
+    write_csv(df_nn, "Dataout/TX_CURR_NN_Calcs.csv", na = "")
